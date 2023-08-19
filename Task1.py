426cec33142e2ab5bcaf6c01af57decd2207a53a
Imię Nazwisko <szymon.radosz@gmail.com>
2023-08-19 11:49:48 +0200

    Add new py def about flatten list

---
+++ Task1.py
@@ -0,0 +1,12 @@
+def flatten_list(nested_list):
+    flattened = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flattened.extend(item)
+        else:
+            flattened.append(item)
+    return flattened
+
+nested_list = [1, 2, [3, 4, [5, [6]], 7], 8, 9, 10]
+flattened_list = flatten_list(nested_list)
+print(flattened_list)
